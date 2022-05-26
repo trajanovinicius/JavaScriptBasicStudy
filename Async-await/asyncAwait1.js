9220d524112a6337fca2b249b7a9da4134ffe04e
trajanovinicius <viniluk9@gmail.com>
2022-05-26 18:10:20 -0300

    littles adjustings in comments

--- Async-await/asyncAwait1.js
+++ Async-await/asyncAwait1.js
@@ -1,4 +1,7 @@
 /* 
 Primeiro, vamos entender o significado da palavra assíncrona:
 que não é sincrôno, que não apresenta sincronia ou sincronismo; assincrônico.
+ou Na área da tecnologia da informação, a comunicação assíncrona é a transmissão de dados, 
+geralmente sem o uso de um sinal de relógio externo, onde os dados podem ser transmitidos 
+intermitentemente em um fluxo estável.
 */
