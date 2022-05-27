ae4df6c3345ce2f5e4b58a503fb96936f6a4b123
trajanovinicius <viniluk9@gmail.com>
2022-05-27 13:23:13 -0300

    finished short summary

--- Async-await/asyncAwait1.js
+++ Async-await/asyncAwait1.js
@@ -4,4 +4,6 @@ que não é sincrôno, que não apresenta sincronia ou sincronismo; assincrônic
 ou Na área da tecnologia da informação, a comunicação assíncrona é a transmissão de dados, 
 geralmente sem o uso de um sinal de relógio externo, onde os dados podem ser transmitidos 
 intermitentemente em um fluxo estável.
+
+Basicamente usando o async você consegue obter dados sem que a página seja carregada
 */
